e47af10ab68b7b446d3e3eb3f22128f5150c434f
nakanuma <k023g0121@g.neec.ac.jp>
2024-06-26 16:17:21 +0900

    Particle用シェーダーの作成

--- Engine/DirectX/DirectXBase.cpp
+++ Engine/DirectX/DirectXBase.cpp
@@ -399,10 +399,10 @@ D3D12_RASTERIZER_DESC DirectXBase::SetRasterizerState()
 void DirectXBase::ShaderCompile()
 {
 	// Shaderをコンパイルする
-	vertexShaderBlob_ = CompileShader(L"resources/Shaders/Object3D.VS.hlsl", L"vs_6_0", dxcUtils_, dxcCompiler_, includeHandler_);
+	vertexShaderBlob_ = CompileShader(L"resources/Shaders/Particle.VS.hlsl", L"vs_6_0", dxcUtils_, dxcCompiler_, includeHandler_);
 	assert(vertexShaderBlob_ != nullptr);
 
-	pixelShaderBlob_ = CompileShader(L"resources/Shaders/Object3D.PS.hlsl", L"ps_6_0", dxcUtils_, dxcCompiler_, includeHandler_);
+	pixelShaderBlob_ = CompileShader(L"resources/Shaders/Particle.PS.hlsl", L"ps_6_0", dxcUtils_, dxcCompiler_, includeHandler_);
 	assert(pixelShaderBlob_ != nullptr);
 }
 
